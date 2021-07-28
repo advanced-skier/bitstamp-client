ac4b77034a238de545fbb4a9fafa141b26f87c23
djuks <djuricastrahinja@gmail.com>
2021-07-28 11:25:33 +0200

    add destination_tag

--- lib/bitstamp/http/withdrawal.rb
+++ lib/bitstamp/http/withdrawal.rb
@@ -6,12 +6,14 @@ module Bitstamp::HTTP
       call(request_uri('v2', 'withdrawal-requests'), 'POST', params)
     end
 
-    def withdrawal(nonce: nil, currency_name:, amount:, address:)
+    def withdrawal(nonce: nil, destination_tag: nil, currency_name:, amount:, address:)
       params = {
         nonce:   nonce,
         amount:  amount,
         address: address
       }
+
+      params.merge!({destination_tag: destination_tag}) if currency_name == 'xrp'
 
       call(request_uri("#{currency_name}_withdrawal"), 'POST', params)
     end
